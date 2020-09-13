4110c59c42371507d02d6e8235ba67770ec6dfff
Igor Sokolov <wasp100wasp100@gmail.com>
2020-09-13 20:28:37 +0300

    Done

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -108,12 +108,52 @@ is_male = {
 }
 
 
-# def flatten(nested):
-    # for dict_ in nested:
+def get_students(school):
+    students = []
+    for group in school:
+        class_id = group['class']
+        for student in group['students'].copy():
+            student['class_id'] = class_id
+            students.append(student)
+
+    return students
 
 
+def assign_gender(students, is_male):
+    students = students.copy()
+    for student in students:
+        name = student['first_name']
+        if is_male[name]:
+            student['gender'] = 'male' 
+        else:
+            student['gender'] = 'female'
+
+    return students
 
 
+def count_gender_by_class(students):
+    gender_count_by_class = {}
+    for student in students:
+        class_id = student['class_id'] 
+        if class_id not in gender_count_by_class:
+            gender_count_by_class[class_id] = {'male': 0, 'female': 0}
+
+        if student['gender'] == 'male':
+            gender_count_by_class[class_id]['male'] += 1
+        else:
+            gender_count_by_class[class_id]['female'] += 1
+
+    return gender_count_by_class
+
+
+students = get_students(school)
+students = assign_gender(students, is_male)
+gender_count_by_class = count_gender_by_class(students)
+for class_id, stats in gender_count_by_class.items():
+    print(INDENT, end='')
+    print(f"В классе {class_id}: девочек = {stats['female']}; мальчиков = {stats['male']}")
+
+print(END)
 # Пример вывода:
 # В классе 2a 2 девочки и 0 мальчика.
 # В классе 3c 0 девочки и 2 мальчика.
@@ -122,8 +162,20 @@ is_male = {
 # Задание 5
 # По информации о учениках разных классов нужно найти класс, в котором больше всего девочек и больше всего мальчиков.
 school = [
-  {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
-  {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
+    {
+        'class': '2a', 
+        'students': [
+            {'first_name': 'Маша'},
+            {'first_name': 'Оля'}
+        ]
+    },
+    {
+        'class': '3c',
+        'students': [
+            {'first_name': 'Олег'}, 
+            {'first_name': 'Миша'}
+        ]
+    },
 ]
 is_male = {
   'Маша': False,
@@ -131,8 +183,30 @@ is_male = {
   'Олег': True,
   'Миша': True,
 }
-# ???
 
 # Пример вывода:
 # Больше всего мальчиков в классе 3c
 # Больше всего девочек в классе 2a
+
+students = get_students(school)
+students = assign_gender(students, is_male)
+gender_count_by_class = count_gender_by_class(students)
+print('нужно найти класс, в котором больше всего девочек и больше всего мальчиков')
+max_male_cnt = 0
+max_female_cnt = 0
+max_male_class = None 
+max_female_class = None
+for class_id, stats in gender_count_by_class.items():
+    if stats['male'] > max_male_cnt:
+        max_male_cnt = stats['male']
+        max_male_class = class_id
+
+    if stats['female'] > max_female_cnt:
+        max_female_cnt = stats['female']
+        max_female_class = class_id
+
+print(INDENT, end='')
+print(f'Больше всего мальчиков в классе {max_male_class}')
+print(INDENT, end='')
+print(f'Больше всего девочек в классе {max_female_class}')
+
